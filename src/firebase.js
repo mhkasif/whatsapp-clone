06c2c9ffd3a813c8feadc279fd046b4527125e78
mhkasif <haseebasif97@gmail.com>
2020-10-24 16:50:42 +0500

    firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC9tfiXjyx9cE-Xw0OJFrifBhrKJ9XyuN8",
+    authDomain: "mhk-whatsapp-clone.firebaseapp.com",
+    databaseURL: "https://mhk-whatsapp-clone.firebaseio.com",
+    projectId: "mhk-whatsapp-clone",
+    storageBucket: "mhk-whatsapp-clone.appspot.com",
+    messagingSenderId: "222917588737",
+    appId: "1:222917588737:web:9d3d74e9fa0e6534118d00",
+    measurementId: "G-GGXN5N4JLW"
+  };
+
